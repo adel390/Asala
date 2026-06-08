fbb6145fc083ad15ecdedab2e70a40c1d96c1a0b
adel390 <113721233+adel390@users.noreply.github.com>
2026-06-08 06:47:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 function checkMeatType() {
+    
     var serviceSelect = document.getElementById("service");
     
+   
     var meatOptions = document.getElementById("meat-options");
+
+    var drinkChoice = document.getElementById("drink-choice");
+
+     var drinklist = document.getElementById("drink-list");
+    
     
     if (serviceSelect.value === "meat") {
         meatOptions.style.display = "block"; 
     } else {
-        meatOptions.style.display = "none";     
+        meatOptions.style.display = "none";  
+    }
+    }
+    function checkDrinkType() {
+
+var drinkChoice = document.getElementById("drink-choice");
+
+     var drinklist = document.getElementById("drink-list");
+
+
+      if (drinkChoice.value === "yes") {
+        drinklist.style.display = "block"; 
+    } else {
+        drinklist.style.display = "none";  
     }
 }
